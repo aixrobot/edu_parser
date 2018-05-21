2076097b02ad951033605445a9cc1f59a777a44b
juseong-yun <yjs8504@naver.com>
2018-05-21 14:12:27 +0900

    .

--- edu_parser/src/edu_parser/DicReader.java
+++ edu_parser/src/edu_parser/DicReader.java
@@ -9,40 +9,40 @@ import java.util.StringTokenizer;
 
 public class DicReader {
 
-	static String filePath = "D://edua_data/engdic/a.dic";
-			
+	static String filePath = "/Users/juseong-yun/IdeaProjects/edu_parser/edu_parser/a.dic";
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// git version 1.0
+			// git version
 
-		/*
-		 * startwith('a')
-		 * regex
-		 * token
-		 * contains
-		 * 
-		 * compare
-		 * equals
-		 * similar
-		 * 
-		 */
-		
-		File file = new File(filePath);
-		try {
-			ReadFileByLine(file);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
+			/*
+			 * startwith('a')
+			 * regex
+			 * token
+			 * contains
+			 *
+			 * compare
+			 * equals
+			 * similar
+			 *
+			 */
+
+			File file = new File(filePath);
+			try {
+				ReadFileByLineSP(file);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+
 	}
 	private static void ReadFileByLine(File fin) throws IOException {
 		FileInputStream fis = new FileInputStream(fin);
-	 
+
 		//Construct BufferedReader from InputStreamReader
 		BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-	 
+
 		String line = null;
 		int i = 0;
 		boolean chk = false;
@@ -69,7 +69,60 @@ public class DicReader {
 			System.out.print("'" + bd +"');");
 			System.out.println("");
 		}
-	 
+
+		br.close();
+	}
+
+	private static void ReadFileByLineSP(File fin) throws IOException {
+		FileInputStream fis = new FileInputStream(fin);
+
+		//Construct BufferedReader from InputStreamReader
+		BufferedReader br = new BufferedReader(new InputStreamReader(fis, "EUC-KR"));
+
+		String line = null;
+
+		String word = "";
+		String wordType = "";
+		String definition = "";
+
+		String [] wordTypeList = {"n,", "a,", "vt,", "vi", "x,"};
+		String [] wordTypeListKor = {"[N]", "[A]", "[V]", "[V]", "[P]"};
+
+		boolean chk = false;
+		while ((line = br.readLine()) != null) {
+			word = "";
+			wordType = "";
+			definition = "";
+
+			String[] splitedAry = line.split("\\:");
+			System.out.println(splitedAry[0]);
+
+			word = splitedAry[0].trim();
+			String rightValue = "";
+			for(int i=1; i<splitedAry.length; i++){
+				rightValue += splitedAry[i];
+			}
+			rightValue = rightValue.trim();
+			System.out.println(rightValue);
+
+
+			for(int j=0; j<wordTypeList.length; j++) {
+				if (rightValue.contains(wordTypeList[j])) {
+					System.out.println("###FINDED : " +wordTypeList[j]+ "#####");
+					//replace
+					rightValue = rightValue.replaceAll(wordTypeList[j], wordTypeListKor[j]);
+					System.out.println(rightValue);
+				}
+			}
+
+			String [] splitedRV = rightValue.split("\\,");
+
+			for(int k=0; k<splitedRV.length; k++){
+				System.out.println(""+splitedRV[k].trim());
+			}
+
+		}
+
 		br.close();
 	}
 }
