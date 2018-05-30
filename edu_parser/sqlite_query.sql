ce2a35555733abe3ff566dfaad8836e93d7b3589
juseong-yun <yjs8504@naver.com>
2018-05-30 18:43:37 +0900

    .

--- edu_parser/sqlite_query.sql
+++ edu_parser/sqlite_query.sql
@@ -5,4 +5,15 @@ select * from TBL_CRAW_KORDIC group by word having count(*) > 1;
 
 select * from TBL_CRAW_KORDIC where word = 'bunk';
 
-select * from TBL_CRAW_KORDIC where id between 100 and 200;
+select * from TBL_CRAW_KORDIC where id between 114624 and 114724;
+
+select * from TBL_CRAW_KORDIC where CRAW_TEXT is null;
+
+delete from TBL_CRAW_KORDIC;
+
+select count(*) from TBL_CRAW_KORDIC;
+
+select count(*) from TBL_CRAW_KORDIC where WORD like 'a%';
+
+select * from TBL_CRAW_KORDIC where WORD like 'a%';
+
